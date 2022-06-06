4194d4e67ad32e1ab01b4f99d32d5697bdc317fe
Sebastian Marambio <hu.weee@gmail.com>
2022-06-06 18:03:50 +0200

    fixed Rating average bug

--- routes/tool.routes.js
+++ routes/tool.routes.js
@@ -16,12 +16,12 @@ router.post("/detail/add", async (req, res, next) => {
     await Tool.create(req.body);
     res.redirect("/user/"); 
   } catch (error) {
-    // if (error.code === 11000) {
-    //     return res
-    //       .status(400)
-    //       .render("/detail/add", { errorMessage: "Program already registered in Database. Please create a program with a different name" });}
+    if (error.code === 11000) {
+        return res
+          .status(400)
+          .render("tools/create", { errorMessage: "Program already registered in Database. Please create a program with a different name" });}
     console.log(error);
-    res.redirect("/tools/detail/add", { errorMessage: "hi!" });
+    res.redirect("/tools/detail/add", { errorMessage: 'Problem!' });
   }
 });
 
@@ -34,9 +34,7 @@ router.get("/detail/:id", async (req, res, next) => {
 router.post("/detail/:id/rate", async (req, res, next) => {
     const {id} = req.params
     const data = await Tool.findById(id)
-    //if (data.numberOfRatings===0) {data.rating=0}
-    const newRating = (((data.rating * data.numberOfRatings) + req.body.rating) / (data.numberOfRatings + 1)).toFixed(1);
-    console.log(newRating)
+    const newRating = (((data.rating * data.numberOfRatings) + Number(req.body.rating)) / (data.numberOfRatings + 1)).toFixed(1);
     const newNumberOfRatings = data.numberOfRatings + 1;
     await Tool.findByIdAndUpdate(id, { rating: newRating, numberOfRatings: newNumberOfRatings });
     res.redirect(`/tools/detail/${id}`); 
